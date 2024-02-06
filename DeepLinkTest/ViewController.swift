9691c8420d61e073a2c3cf797d26229076c589de
Artem Golikov <golikovartem404@gmail.com>
2024-02-06 11:33:37 +0100

    Initial Commit

---
+++ DeepLinkTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeepLinkTest
+//
+//  Created by Artem Golikov on 06.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
